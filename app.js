ebc8f3c7eecda76e96162d3cabd43c55cbc2a4a9
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-09-17 18:14:28 +0100

    Add basic button code

---
+++ app.js
@@ -0,0 +1,7 @@
+const publishBtn = document.getElementById("publish-btn")
+const endorsementTextEl = document.getElementById("endoresement-text")
+const endorsementListEl = document.getElementById("endorsement-list")
+
+publishBtn.addEventListener("click", () => {
+    console.log("Click!")
+})
