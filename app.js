029fede943f9c88c0e3d37abcbbb887d3ba83500
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-09-17 19:32:24 +0100

    Add push to DB on click functionality

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-c8093-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsementsInDB = ref(database, "endorsements")
+
 const publishBtn = document.getElementById("publish-btn")
-const endorsementTextEl = document.getElementById("endoresement-text")
+const endorsementTextEl = document.getElementById("endorsement-text")
 const endorsementListEl = document.getElementById("endorsement-list")
 
 publishBtn.addEventListener("click", () => {
-    console.log("Click!")
+    let textInput = endorsementTextEl.value
+    push(endorsementsInDB, textInput)
+
+    clearEndorsementTextEl()
 })
+
+function clearEndorsementTextEl() {
+    endorsementTextEl.value = ""
+}
