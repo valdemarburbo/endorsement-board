a5f57f7666989c1f5cbf37956141cebd30e7ae25
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-09-18 01:59:07 +0100

    Add to/from input fields and make it appear on endorsements

--- app.js
+++ app.js
@@ -9,17 +9,60 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const endorsementsInDB = ref(database, "endorsements")
 
+onValue(endorsementsInDB, (snapshot) => {
+    if (snapshot.exists()) {
+        let endorsementArray = Object.entries(snapshot.val())
+
+        clearEndorsementListEl()
+        
+        for (let i = 0; i < endorsementArray.length; i++) {
+            let currentEndorsement = endorsementArray[i]
+
+            appendEndorsementToListEl(currentEndorsement)
+        }
+    }
+})
+
 const publishBtn = document.getElementById("publish-btn")
 const endorsementTextEl = document.getElementById("endorsement-text")
 const endorsementListEl = document.getElementById("endorsement-list")
+const fromInputEl = document.getElementById("from-input")
+const toInputEl = document.getElementById("to-input")
 
 publishBtn.addEventListener("click", () => {
-    let textInput = endorsementTextEl.value
-    push(endorsementsInDB, textInput)
+    let inputObject = {
+        text: endorsementTextEl.value,
+        from: fromInputEl.value,
+        to: toInputEl.value,
+    }
 
-    clearEndorsementTextEl()
+    push(endorsementsInDB, inputObject)
+
+    clearEndorsementInputFields()
 })
 
-function clearEndorsementTextEl() {
+function clearEndorsementInputFields() {
     endorsementTextEl.value = ""
+    fromInputEl.value = ""
+    toInputEl.value = ""
+}
+
+function clearEndorsementListEl() {
+    endorsementListEl.innerHTML = ""
+}
+
+function appendEndorsementToListEl(endorsement) {
+    let endorsementID = endorsement[0]
+    let endorsementValue = endorsement[1].text
+    let endorsementFrom = endorsement[1].from
+    let endorsementTo = endorsement[1].to
+
+    let newEl = document.createElement("li")
+    newEl.innerHTML = `
+        <h3>To ${endorsementTo}</h3>
+        <p>${endorsementValue}</p>
+        <h3>From ${endorsementFrom}</h3>
+    `
+
+    endorsementListEl.append(newEl)
 }
